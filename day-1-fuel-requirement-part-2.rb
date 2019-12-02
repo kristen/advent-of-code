63f9d01e87f7e7e018c32c3596807cee4ddfac37
Kristen Sundquist <kssundquist@gmail.com>
2019-12-02 07:29:12 -0800

    Day 1 - Part 2

---
+++ day-1-fuel-requirement-part-2.rb
@@ -0,0 +1,24 @@
+input = File.read('day-1-input').split("\n").map(&:to_i)
+
+def get_fuel_required mass
+  (mass / 3).floor - 2
+end
+
+def get_fuel_required_recursive mass
+  def loop fuel_acc, rest_mass
+    fuel = get_fuel_required rest_mass
+    puts "fuel: #{fuel}"
+    if fuel <= 0
+      fuel_acc
+    else
+      loop(fuel_acc + fuel, fuel)
+    end
+  end
+  loop(0, mass)
+end
+
+puts get_fuel_required_recursive 14
+puts get_fuel_required_recursive 1969
+
+result = input.map { |mass| get_fuel_required_recursive mass }.sum
+puts result
