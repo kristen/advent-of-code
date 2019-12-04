847e431010df0648a87021fcc9ee64c11636c4bf
Kristen Sundquist <kssundquist@gmail.com>
2019-12-03 22:06:19 -0800

    Day 3 - Part 2

--- day-3/day-3-part-2.rb
+++ day-3/day-3-part-2.rb
@@ -1,16 +1,3 @@
-
-
-# find manhattan distance
-
-# do we just find the intersection of any of the lines?
-# How do we find intersections in math?
-# These are segments, not lines
-# central port is point (0, 0)
-
-# nested loop
-# check if line segments intersect
-# if they intersect, find manhatan distance from (0, 0) of intersection
-
 class Point
   attr_accessor :x, :y
   def initialize x, y
@@ -56,8 +43,6 @@ class Segment
       max_y = [other.point1.y, other.point2.y].max
       min_y = [other.point1.y, other.point2.y].min
       # puts "max_x: #{max_x}; min_x: #{min_x}; max_y: #{max_y}; min_y: #{min_y};"
-      
-
 
       if other.point1.x <= max_x && other.point1.x >= min_x && other.point2.x <= max_x && other.point2.x >= min_x &&
         point1.y <= max_y && point1.y >= min_y && point2.y <= max_y && point2.y >= min_y
@@ -76,19 +61,6 @@ class Segment
     "[p1: #{point1} p2: #{point2}]"
   end
 end
-
-def test_segment_intersect s1, s2, expected
-  result = s1.intersection s2
-  puts "match: #{result == expected}; expected: #{expected}; #{s1}.intersection #{s2} = #{result}"
-end
-
-# segment1 = Segment.new(Point.new(3, 2), Point.new(3, 4))
-# segment2 = Segment.new(Point.new(1, 3), Point.new(4, 3))
-# test_segment_intersect segment1, segment2, Point.new(3, 3)
-
-# segment1 = Segment.new(Point.new(8, 5), Point.new(3, 5))
-# segment2 = Segment.new(Point.new(6, 7), Point.new(6, 3))
-# test_segment_intersect segment1, segment2, Point.new(6, 5)
 
 UP = "U"
 DOWN = "D"
@@ -120,52 +92,69 @@ def segments_from_path path
   result
 end
 
-def test_segments_from_path path, expected
-  result = segments_from_path path
-  puts "match: #{result == expected}; expected: #{expected}; segments_from_path(#{path}) = #{result}"
-end
-
-# test_segments_from_path ["R8", "U5", "L5", "D3"], [
-#   Segment.new(Point.new(0, 0), Point.new(8, 0)),
-#   Segment.new(Point.new(8, 0), Point.new(8, 5)),
-#   Segment.new(Point.new(8, 5), Point.new(3, 5)),
-#   Segment.new(Point.new(3, 5), Point.new(3, 2))
-# ]
-
-def manhattan_distance path1, path2
-  closest_intersection = nil
-
+def get_fewest_steps path1, path2
+  shortest_steps = nil
   # iterate through paths to find pairs of x,y corrdinates that make a segment
 
   segments_path1 = segments_from_path path1
   segments_path2 = segments_from_path path2
 
+  # iterate through combinations of segments in order
+  # when you hit first intersection, add up steps prior to that i, j and return
   for i in 0..segments_path1.length-1
     for j in i..segments_path2.length-1
       s1 = segments_path1[i]
       s2 = segments_path2[j]
+      puts "cur move #{path1[i]} s1: #{s1}"
+      puts "cur move #{path2[j]} s2: #{s2}"
       intersection = s1.intersection(s2)
-      if intersection && i != 0
+      if intersection
         puts "intersection: #{intersection}"
-        distance = intersection.taxicab_distance Point.new(0, 0)
-        puts "distance: #{distance}"
-        closest_intersection = distance if closest_intersection.nil? || distance < closest_intersection
+        moves1 = path1[0..i-1]
+        moves2 = path2[0..j-1]
+
+        steps_before_intersection = moves1.map { |move| move[1..move.length-1].to_i }.sum +
+          moves2.map{ |move| move[1..move.length-1].to_i }.sum
+
+        prev1 = s1.point1
+        curr_move1 = path1[i][0]
+
+        curr_step1 = case curr_move1
+        when UP, DOWN
+          # delta of prev1.y with intersection.y
+          (prev1.y - intersection.y).abs
+        when LEFT, RIGHT
+          (prev1.x - intersection.x).abs
+        end
+
+        prev2 = s2.point1
+        curr_move2 = path2[j][0]
+        curr_step2 = case curr_move2
+        when UP, DOWN
+          # delta of prev1.y with intersection.y
+          (prev2.y - intersection.y).abs
+        when LEFT, RIGHT
+          (prev2.x - intersection.x).abs
+        end
+
+        steps = steps_before_intersection + curr_step1 + curr_step2
+        shortest_steps = steps if shortest_steps.nil? || steps < shortest_steps
       end
     end
   end
 
-  closest_intersection
+  shortest_steps
 end
 
 def test path1, path2, expected
-  result = manhattan_distance path1, path2
-  puts "match: #{result == expected}; expected: #{expected}; manhattan_distance(#{path1}, #{path2}) = #{result}"
+  result = get_fewest_steps path1, path2
+  puts "match: #{result == expected}; expected: #{expected}; get_fewest_steps(#{path1}, #{path2}) = #{result}"
 end
+test ["R8", "U5", "L5"], ["U7", "R6", "D4"], 30
+# test ["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"], 30
+# test ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"], ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"], 610
+# test ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"], ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"], 410
 
-test ["R8", "U5", "L5"], ["U7", "R6", "D4"], 11
-test ["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"], 6
-test ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"], ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"], 159
-test ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"], ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"], 135
 
-# path1, path2 = File.read('day-3-input').split("\n").map { |l| l.split(",") }
-# puts manhattan_distance path1, path2
+path1, path2 = File.read('day-3-input').split("\n").map { |l| l.split(",") }
+puts get_fewest_steps path1, path2
