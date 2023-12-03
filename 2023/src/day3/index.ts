87c38b66f575aacf30a682e65dd9cdb16498baa8
Kristen Sundquist <kssundquist@gmail.com>
2023-12-02 23:03:12 -0800

    2023 day 3 part 2

--- 2023/src/day3/index.ts
+++ 2023/src/day3/index.ts
@@ -21,6 +21,10 @@ const isSymbol = (char: string): boolean => {
     return !char.match(/\d|\./)
 }
 
+const isGear = (char: string): boolean => {
+    return char === "*";
+}
+
 const isNumber = (char: string): boolean => {
     return !!char.match(/\d/);
 }
@@ -29,13 +33,19 @@ const containsSymbol = (chars: string[]): boolean => {
     return chars.some((char) => isSymbol(char));
 }
 
+const containsGear = (chars: { char: string; k: number; l: number }[]): { char: string; k: number; l: number } | undefined => {
+    return chars.find(({ char }) => {
+        return isGear(char);
+    });
+}
+
 const isValidPos = (n: number, m: number) => (i: number, j: number): boolean => {
     if (i < 0 || j < 0 || i > n - 1 || j > m - 1)
         return false;
     return true;
 }
 
-const getAdjacent = (lines: string[], i: number, j: number): string[] => {
+const getAdjacent = (lines: string[], i: number, j: number): { char: string; k: number; l: number }[] => {
     // boundaries
     const n = lines.length;
     const m = lines[0].length;
@@ -55,41 +65,12 @@ const getAdjacent = (lines: string[], i: number, j: number): string[] => {
 
     return adjacentIndicies.flatMap(([k, l]) => {
         if (isValid(k, l)) {
-            return [lines[k][l]];
+            return [{ char: lines[k][l], k, l }];
         }
         return [];
     })
 }
 
-// given a position i, j
-// is there a symbol touching it
-// don't check out of bounds
-// numbers can be more than 1 char long
-
-// const findPartNumbers = (line: string): number[] => {
-//     for (let i = 0; i < line.length; i++) {
-//         const char = line[i];
-//         const adjacentChar = getAdjacent()
-
-//     }
-//     return [...line.matchAll(/(\d+)/g)].reduce((acc, match, i) => {
-//         const [numberStr, _, startsAt] = match;
-//         // see if symbol is adjacent
-//         const endAt = startsAt + numberStr.length;
-//         console.log({
-//             numberStr,
-//             startsAt,
-//             endAt,
-//         });
-
-//         for (let j = 0; j < numberStr.length; j++) {
-//             // if i check row above
-
-//         }
-
-//         return acc;
-//     }, [])
-// }
 
 const part1 = (fileName: string) => {
     const input = parseInput(fileName);
@@ -114,7 +95,7 @@ const part1 = (fileName: string) => {
                 if (!hasAdjacentSymbol) {
                     const adjacentChars = getAdjacent(input, i, j);
                     console.log("adjacentChars", adjacentChars)
-                    if (containsSymbol(adjacentChars)) {
+                    if (containsSymbol(adjacentChars.map(({ char }) => char))) {
                         console.log("has adjacent symbol", { char })
                         hasAdjacentSymbol = true;
                     }
@@ -141,4 +122,67 @@ const part1 = (fileName: string) => {
     console.log("result", partNumbers.reduce((acc, n) => acc + n), 0);
 }
 
-part1('input')
+
+// iterate through lines
+// if find gear, look at adjacent numbers
+// if exactly 2 adjacent number, find full numbers and multiply
+// sum up all numbers
+
+const part2 = (fileName: string) => {
+    const input = parseInput(fileName);
+    // store numbers adjacent to gears
+    // gears identified by position
+    const gearParts: { [gearLocation: string]: number[] } = {};
+    for (let i = 0; i < input.length; i++) {
+        const line = input[i];
+        console.log("---line---")
+        console.log(line)
+        let partNumber = "";
+        let gearLocation = "";
+        for (let j = 0; j < line.length; j++) {
+            const char = line[j];
+            if (isNumber(char)) {
+                console.log("is number", {
+                    partNumber,
+                    char,
+                    i,
+                    j,
+                    length: line.length
+                })
+                partNumber = partNumber + char;
+                const adjacentChars = getAdjacent(input, i, j);
+                console.log("adjacentChars", adjacentChars)
+                const maybeGear = containsGear(adjacentChars);
+                if (maybeGear) {
+                    gearLocation = `(${maybeGear.k}, ${maybeGear.l})`;
+                    console.log("has adjacent GEAR", { char, gearLocation })
+                }
+            }
+            // if not number anymore or at end of line
+            if (!isNumber(char) || j === line.length - 1) {
+                console.log('NOT number anymore', { char, partNumber, gearLocation })
+                if (gearLocation && partNumber) {
+                    const number = parseInt(partNumber, 10);
+                    console.log("SAVING part number", number)
+                    gearParts[gearLocation] = gearParts[gearLocation] ?? [];
+                    gearParts[gearLocation].push(number);
+                } else {
+                    if (partNumber) {
+                        console.log("NOT saving part", parseInt(partNumber, 10));
+                    }
+                }
+                partNumber = "";
+                gearLocation = "";
+            }
+        }
+    }
+    console.log("part2", gearParts)
+    const validGears = Object.values(gearParts).filter((parts) => {
+        return parts.length === 2;
+    })
+    console.log("validGears", validGears)
+    const result = validGears.map(([p1, p2]) => p1 * p2).reduce((sum, ratio) => sum + ratio, 0);
+    console.log("result", result);
+}
+
+part2('input')
